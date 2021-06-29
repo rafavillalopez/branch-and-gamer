d92eb204e2b12f46c6889af98f3110b093a29246
ivan-cardoso <ivancardoso59@gmail.com>
2021-06-29 19:16:30 -0300

    Products routes

---
+++ api/routes/products.js
@@ -0,0 +1,54 @@
+const express = require("express")
+const router = express.Router()
+const { Product } = require("../models")
+
+
+router.get("/", (req, res, next) => {
+    Product.findAll()
+    .then((data) => {
+        res.status(200).json(data)
+    })
+    .catch(next)
+})
+
+router.get("/:id", (req, res, next) => {
+    Product.findByPk(req.params.id)
+    .then((data) => {
+        res.status(200).json(data)
+    })
+    .catch(next)
+})
+
+router.post("/", (req, res, next) => {
+    Product.create(req.body)
+    .then((data) => {
+        res.status(201).json(data)
+    })
+    .catch(next)
+})
+
+router.put("/:id", (req, res, next) => {
+    Product.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(() => {
+        res.sendStatus(200)
+    })
+    // DEBERIAMOS DEVOLVER EL PRODUCTO ACTUALIZADO?
+    .catch(next)
+})
+
+router.delete("/:id", (req, res, next) => {
+    Product.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(() => {
+        res.sendStatus(200)
+    })
+    .catch(next)
+})
+
--- api/server.js
+++ api/server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const morgan = require("morgan");
 
 const db = require("./db");
+const routes = require("./routes")
 const app = express();
+
 
 // LOGGUER
 app.use(morgan("dev"));
@@ -11,11 +13,19 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// ROUTES
+app.use("/api", routes)
+
 // RUTA PRUEBA
 app.use("/", (req, res, nex) => {
   res.send("ESTAS EN '/'");
 });
 
+app.use("/", (err, req, res, next)=>{
+  console.log(err)
+  console.error("ERROR", err.stack)
+  res.status(500).json(err)
+})
 
 
 const PORT = 3001;
