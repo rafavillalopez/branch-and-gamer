2eee8a04ae171714858d4bad8c9a082c7a706fe7
Rafael Villa <Rafita@192.168.1.13>
2021-07-01 00:59:10 -0300

    rutas del carrito terminadas

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -3,8 +3,14 @@ const { Carrito } = require("../models");
 const ProducstCarrito = require("../models/ProducstCarrito");
 const router = express.Router();
 
+/* 
+
+Estas rutas solo deberian estar disponibles para un 
+usurio logueado, por eso al entrar deberian aprobar un middleware de JWT y asegurarse de que tienen un token valido
+
+*/
+
 router.post("/add", (req, res, next) => {
-  console.log("BAD");
   const { carritoId, productId } = req.body;
   ProducstCarrito.findOrCreate({
     where: {
@@ -19,9 +25,8 @@ router.post("/add", (req, res, next) => {
 });
 
 router.post("/:userId", (req, res, next) => {
-  console.log("GOOD");
   const { userId } = req.params;
-  console.log(req.params)
+  console.log(req.params);
   Carrito.findOrCreate({
     where: {
       userId,
@@ -32,19 +37,47 @@ router.post("/:userId", (req, res, next) => {
   });
 });
 
-router.post("/delete", (req, res, next) => {
-  const { carritoId, productId } = req.body;
-  ProducstCarrito.findOrCreate({
-    carritoId,
-    productId,
+router.delete("/:carritoId/:productId", (req, res, next) => {
+  const { carritoId, productId } = req.params;
+  ProducstCarrito.destroy({
+    where: {
+      carritoId,
+      productId,
+    },
+  })
+    .then((response) => {
+      res.json({ msg: `Deleted ${response} product from ${carritoId}` });
+    })
+    .catch(next);
+});
+
+router.put("/", (req, res, next) => {
+  /* En esta ruta se pueden agregar o remover uno (1) de un mismo producto en el carrito, necesita una key "type: 'add'", o type: 'remove', que define el metodo a usar, tambien necesita el carritoId y el productId en el body
+  
+  Ejemplo: 
+
+{
+  "carritoId": 1,
+  "productId" : 3,
+  "type": "remove" / "add"
+} 
+  
+  */
+
+  const { carritoId, productId, type } = req.body;
+
+  ProducstCarrito.findOne({
+    where: {
+      carritoId,
+      productId,
+    },
   })
     .then((productInCar) => {
-      console.log(productInCar);
-      // return productInCar.add(1);
+      return productInCar[type]();
     })
-    // .then((updated) => {
-    //   res.json(updated);
-    // })
+    .then((updated) => {
+      res.sendStatus(204);
+    })
     .catch(next);
 });
 
