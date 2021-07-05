62873a3a4a67b4b07873a4e11174ef01294b5be7
tvallejoss <tvallejos@stgroup.com.ar>
2021-07-05 18:51:15 -0300

    arreglo de botones de singleProducts

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -10,62 +10,77 @@ import { getProductFromDbAndSetQuantities } from "../utils";
 import "../assets/cart.css";
 
 export default function Cart() {
+    const { cartItems } = useSelector((state) => state);
 
-  const { cartItems } = useSelector((state) => state);
+    const [itemsToRender, setItemsToRender] = useState([]);
 
-  const [itemsToRender, setItemsToRender] = useState([]);
+    let orderTotal = 0;
 
-  let orderTotal = 0;
+    useEffect(() => {
+        getProductFromDbAndSetQuantities(cartItems).then((dbproducts) => {
+            setItemsToRender(dbproducts);
+        });
+    }, [cartItems]);
 
-  useEffect(() => {
-    getProductFromDbAndSetQuantities(cartItems).then((dbproducts) => {
-      setItemsToRender(dbproducts);
-    });
-  }, [cartItems]);
-
-  return (
-    <div>
-      <div className="cart_section">
-        <div className="container-fluid" />
-        <div className="row">
-          <div className="col-lg-10 offset-lg-1">
-            <div className="cart_container">
-              <div className="cart_title">
-                Shopping Cart
-                <small>
-                  {" "}
-                  ({itemsToRender ? itemsToRender.length : 1} item/s in your
-                  cart){" "}
-                </small>
-              </div>
-              <div className="cart_items">
-                <ul className="cart_list">
-                  {itemsToRender?.map((producto, i) => {
-                    orderTotal += producto.price * producto.quantity;
-                    return <CartItems key={i} producto={producto} />;
-                  })}
-                </ul>
-              </div>
-              <div className="order_total">
-                <div className="order_total_content text-md-right">
-                  <div className="order_total_title">Order Total:</div>
-                  <div className="order_total_amount">{`$${orderTotal}`}</div>
+    return (
+        <div>
+            <div className="cart_section">
+                <div className="container-fluid" />
+                <div className="row">
+                    <div className="col-lg-10 offset-lg-1">
+                        <div className="cart_container">
+                            <div className="cart_title">
+                                Shopping Cart
+                                <small>
+                                    {" "}
+                                    ({itemsToRender
+                                        ? itemsToRender.length
+                                        : 1}{" "}
+                                    item/s in your cart){" "}
+                                </small>
+                            </div>
+                            <div className="cart_items">
+                                <ul className="cart_list">
+                                    {itemsToRender?.map((producto, i) => {
+                                        orderTotal +=
+                                            producto.price * producto.quantity;
+                                        return (
+                                            <CartItems
+                                                key={i}
+                                                producto={producto}
+                                            />
+                                        );
+                                    })}
+                                </ul>
+                            </div>
+                            <div className="order_total">
+                                <div className="order_total_content text-md-right">
+                                    <div className="order_total_title">
+                                        Order Total:
+                                    </div>
+                                    <div className="order_total_amount">{`$${orderTotal}`}</div>
+                                </div>
+                            </div>
+                            <div className="cart_buttons">
+                                <Link to="/">
+                                    <button
+                                        type="button"
+                                        className="button cart_button_clear"
+                                    >
+                                        Seguir Comprando
+                                    </button>
+                                </Link>
+                                <button
+                                    type="button"
+                                    className="button cart_button_checkout"
+                                >
+                                    Confirmar Compra
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div className="cart_buttons">
-                <Link to="/">
-                  <button type="button" className="button cart_button_clear">
-                    Continue Shopping
-                  </button>
-                </Link>
-                <button type="button" className="button cart_button_checkout">
-                  Confirmar Compra
-                </button>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
