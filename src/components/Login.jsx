799f7e51acee6dc5d0167885e49d3439c0ac94cd
Alejandro Zuñiga <alejandrozuniga@MacBook-Pro-de-Alejandro.local>
2021-07-05 12:32:42 -0300

    vista_header

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -39,67 +39,67 @@ export default function Login() {
 
   return (
     <div>
-    <div>
-      <Navbar />
-      <div class="register">
-        <Link
-          to="/"
-          className="goback"
-          style={{ textDecoration: "none" }}
-        >
-          Volver
+      <div>
+        <Navbar />
+        {/* <div class=""> */}
+          <Link
+            to="/"
+            className="goback"
+            style={{ textDecoration: "none" }}
+          >
+            Volver
         </Link>
-      <div className='register-card'>
-        
-        <form onSubmit={onSubmit}>
-          <div className="log-text">Login</div>
-          <div className="form-group d-flex align-items-center">
-            <div className="icon">
-              <span class="far fa-envelope"></span>
-            </div>{" "}
-            <input
-              onChange={onChange}
-              autoComplete="off"
-              type="email"
-              placeholder="Email"
-              name="email"
-            />
+          <div className='register-card'>
+
+            <form onSubmit={onSubmit}>
+              <div className="log-text">Login</div>
+              <div className="form-group d-flex align-items-center">
+                <div className="icon">
+                  <span class="far fa-envelope"></span>
+                </div>{" "}
+                <input
+                  onChange={onChange}
+                  autoComplete="off"
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                />
+              </div>
+              <div className="form-group d-flex align-items-center">
+                <div className="icon">
+                  <span className="fas fa-key"></span>
+                </div>{" "}
+                <input
+                  onChange={onChange}
+                  autoComplete="off"
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                />
+              </div>
+              {alert && (
+                <Alert
+                  style={{
+                    borderRadius: "30px",
+                    fontSize: "12px",
+                    height: "12px",
+                    paddingTop: 0,
+                    paddingBottom: "17px",
+                  }}
+                  variant={"danger"}
+                >
+                  Email o contraseña inválidos
+                </Alert>
+              )}
+              <input type="submit" value="Login" className="log-btn" />
+              <h6> ¿No tienes cuenta? ¡Click aquí!</h6>
+              <Link to="/register">
+                <button className="register-btn">Registrarme</button>
+              </Link>
+            </form>
           </div>
-          <div className="form-group d-flex align-items-center">
-            <div className="icon">
-              <span className="fas fa-key"></span>
-            </div>{" "}
-            <input
-              onChange={onChange}
-              autoComplete="off"
-              type="password"
-              placeholder="Password"
-              name="password"
-            />
-          </div>
-          {alert && (
-            <Alert
-              style={{
-                borderRadius: "30px",
-                fontSize: "12px",
-                height: "12px",
-                paddingTop: 0,
-                paddingBottom: "17px",
-              }}
-              variant={"danger"}
-            >
-              Email o contraseña inválidos
-            </Alert>
-          )}
-          <input type="submit" value="Login" className="log-btn" />
-          <h6> ¿No tienes cuenta? ¡Click aquí!</h6>
-          <Link to="/register">
-            <button className="register-btn">Registrarme</button>
-          </Link>
-        </form>
+        {/* </div> */}
       </div>
-    </div>
-    </div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,65 +1,90 @@
 import * as React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
+import { setUserVoid } from "../store/loggedUserReducer";
 
 export default function Navbar() {
-  const history = useHistory();
+    const history = useHistory();
+    const dispatch = useDispatch();
 
-  const inicio = () => {
-    history.push("/");
-  };
+    let { isLogIn } = useSelector((state) => state);
 
-  return (
-    <div>
-      <nav className="navbar navbar-expand-sm navbar-light bg-white border-bottom">
-        {" "}
-        <button className="logo-btn" onClick={inicio}>
-          <img
-            className="logo"
-            src="https://i.postimg.cc/MK3vy7xt/Captura-de-Pantalla-2021-07-01-a-la-s-01-10-58.png"
-            alt="Branch&Gamer"
-          />
-        </button>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarColor"
-          aria-controls="navbarColor"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          {" "}
-          <span className="navbar-toggler-icon"></span>{" "}
-        </button>
-        <div className="collapse navbar-collapse" id="navbarColor">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <div className="nav-link" href="#">
-                <Link to="/login">
-                  <span class="fa fa-user-o"></span>
-                  <img
-                    src="https://img.icons8.com/ios-filled/30/000000/stormtrooper.png"
-                    alt=""
-                  />
-                  <span class="text">Login</span>
-                </Link>
-              </div>{" "}
-            </li>
-            <li className="nav-item ">
-              <div className="nav-link" href="#">
-                <Link to="/cart">
-                  <span class="fa fa-shopping-cart"></span>
-                  <img
-                    src="https://img.icons8.com/ios-glyphs/30/000000/fast-cart.png"
-                    alt=""
-                  />
-                  <span class="textCart">Carrito</span>
-                </Link>
-              </div>{" "}
-            </li>
-          </ul>
+    function logOut() {
+        window.localStorage.removeItem("branchToken");
+        dispatch(setUserVoid());
+    }
+    const inicio = () => {
+        history.push("/");
+    };
+
+    return (
+        <div>
+            <nav className="navbar navbar-expand-sm navbar-light bg-white border-bottom">
+                {" "}
+                <button className="logo-btn" onClick={inicio}>
+                    <img
+                        className="logo"
+                        src="https://i.postimg.cc/MK3vy7xt/Captura-de-Pantalla-2021-07-01-a-la-s-01-10-58.png"
+                        alt="Branch&Gamer"
+                    />
+                </button>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarColor"
+                    aria-controls="navbarColor"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    {" "}
+                    <span className="navbar-toggler-icon"></span>{" "}
+                </button>
+                <div className="collapse navbar-collapse" id="navbarColor">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            {!isLogIn ? (
+                                <div className="nav-link">
+                                    <Link to="/login">
+                                        <span className="fa fa-user-o p-0"></span>
+                                        <img
+                                            src="https://img.icons8.com/ios-filled/30/000000/stormtrooper.png"
+                                            alt=""
+                                        />
+                                        <span className="text">Login</span>
+                                    </Link>
+                                </div>
+                            ) : (
+                                <div className="nav-link">
+                                    <button
+                                        className="log-out-button"
+                                        onClick={logOut}
+                                    >
+                                        <span className="fa fa-user-o p-0"></span>
+                                        <img
+                                            src="https://img.icons8.com/ios-filled/30/000000/stormtrooper.png"
+                                            alt=""
+                                        />
+                                        <span className="text">LogOut</span>
+                                    </button>
+                                </div>
+                            )}
+                        </li>
+                        <li className="nav-item ">
+                            <div className="nav-link" href="#">
+                                <Link to="/cart">
+                                    <span class="fa fa-shopping-cart"></span>
+                                    <img
+                                        src="https://img.icons8.com/ios-glyphs/30/000000/fast-cart.png"
+                                        alt=""
+                                    />
+                                    <span class="textCart">Carrito</span>
+                                </Link>
+                            </div>{" "}
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
-      </nav>
-    </div>
-  );
+    );
 }
